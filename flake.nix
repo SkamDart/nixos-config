9e853d6ee2703948507e11d6fb87b46f822cbb8d
Cameron Dart <cdart2@illinois.edu>
2023-11-21 06:15:20 +0000

    export stinky@jellybean through homeConfigurations

--- flake.nix
+++ flake.nix
@@ -84,15 +84,15 @@
 
       # Standalone home-manager configuration entrypoint
       # Available through 'home-manager --flake .#stinky@jellybean'
-      # homeConfigurations = {
-      #   "stinky@jellybean" = home-manager.lib.homeManagerConfiguration {
-      #     pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-      #     extraSpecialArgs = {inherit inputs outputs;};
-      #     modules = [
-      #       # > Our main home-manager configuration file <
-      #       ./home-manager/home.nix
-      #     ];
-      #   };
-      # };
+      homeConfigurations = {
+        "stinky@jellybean" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = {inherit inputs outputs;};
+          modules = [
+            # > Our main home-manager configuration file <
+            ./home-manager/home.nix
+          ];
+        };
+      };
     };
 }
