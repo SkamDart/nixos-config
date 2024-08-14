6653dc09c3c9212cd12fd92eaa39267da324fbe4
Cameron Dart <8763518+SkamDart@users.noreply.github.com>
2024-08-14 18:48:12 +0000

    24.05 update

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
-  description = "Your new nix config";
+  description = "My fancy nixos configuration";
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -12,7 +12,7 @@
     ghostty.url = "git+ssh://git@github.com/ghostty-org/ghostty";
 
     # Home manager
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     neovim-nightly-overlay = {
